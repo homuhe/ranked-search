2180b5a45a6862d5b2cf3e1fdbd53bb48e340065
Holger <homuhe@gmail.com>
2017-01-04 14:27:26 +0100

    Initial commit:  - in console: mvn archetype:generate  - searched for "net.alchim31.maven simple-version"  - typed in "com.ir" groupID  - typed in "ranked-search" artifactID  - imported maven project in IntelliJ  - shared project over menu tab "VCS" -> "share on Github"

---
+++ README.md
@@ -0,0 +1,23 @@
+# ranked-search
+HS Information Retrieval WS16/17 - Assignment 3
+
+
+Search engine that uses a vector model to query the English Reuters 21578 corpus.
+
+
+
+**Usage:**
+- 1.1 NaN
+```
+./NaN arg1 arg2
+  arg1: INPUT - ...
+  arg2: OUTPUT - ...
+```
+ 
+ 
+  
+  
+  
+Authors:
+
+Alexander Hartmann, Holger Muth-Hellebrandt
---
+++ src/main/scala/com/ir/RankedSearch.scala
@@ -0,0 +1,30 @@
+package com.ir
+
+/** Author:       Alexander Hartmann,
+  *               Holger Muth-Hellebrandt
+  *
+  * Task:         Assignment 3
+  * Description:  Processes query of user input on inverted indices file.
+  */
+
+object RankedSearch {
+
+  /**
+    * Main method
+    */
+  def main(args: Array[String]): Unit = {
+
+    println("Harambe!")
+
+  }
+
+
+  /**
+    * Help function for correct usage
+    */
+  def help() = {
+    println("Help function")
+    sys.exit()
+  }
+
+}
